3ab691ef170c5fced0f267a4e248c1580b3108e4
lijinlong <303882175@qq.com>
2022-02-13 02:05:58 +0800

    将开头*号改成-号

--- day75/代码/BBS14/static/kindeditor/plugins/multiimage/multiimage.js
+++ day75/代码/BBS14/static/kindeditor/plugins/multiimage/multiimage.js
@@ -1,10 +1,10 @@
 /*******************************************************************************
-* KindEditor - WYSIWYG HTML Editor for Internet
-* Copyright (C) 2006-2011 kindsoft.net
+- KindEditor - WYSIWYG HTML Editor for Internet
+- Copyright (C) 2006-2011 kindsoft.net
 *
-* @author Roddy <luolonghao@gmail.com>
-* @site http://www.kindsoft.net/
-* @licence http://www.kindsoft.net/license.php
+- @author Roddy <luolonghao@gmail.com>
+- @site http://www.kindsoft.net/
+- @licence http://www.kindsoft.net/license.php
 *******************************************************************************/
 
 
